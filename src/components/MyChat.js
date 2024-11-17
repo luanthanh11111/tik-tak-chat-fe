7e78be4c8ab3a0e735cb1bcb4f5473f021693bd3
luan.khuc <luan.khuc@onemount.com>
2024-11-17 22:31:29 +0700

    change post

--- src/components/MyChat.js
+++ src/components/MyChat.js
@@ -22,7 +22,7 @@ function MyChat({ fetchAgain }) {
       };
 
       const { data } = await axios.get(
-        "http://localhost:4000/api/chat",
+        "https://tik-tak-chat-be.onrender.com/api/chat",
         config
       );
       setChats(data);
--- src/components/ScrollableChat.js
+++ src/components/ScrollableChat.js
@@ -40,7 +40,7 @@ function ScrollableChat({ messages, setFetchAgain, fetchAgain, fetchMessage }) {
         },
       };
       await axios.put(
-        `http://localhost:4000/api/message/remove/${id}`,
+        `https://tik-tak-chat-be.onrender.com/api/message/remove/${id}`,
         { userId: user._id },
         config
       );
