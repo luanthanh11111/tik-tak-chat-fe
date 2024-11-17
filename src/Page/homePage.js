1a907f675b7df84be51ee3adf1d9bdbca8a9b5e0
luan.khuc <luan.khuc@onemount.com>
2024-11-17 22:40:34 +0700

    hjk

--- src/Page/homePage.js
+++ src/Page/homePage.js
@@ -36,7 +36,7 @@ const HomePage = () => {
         style={{ backgroundImage: "transparent", backdropFilter: "blur(10px)" }}
       >
         <Text fontFamily="Amatic SC" fontSize="5xl">
-          Chat app by vợ Loan
+          Chat app by ngu
         </Text>
       </Box>
       <Box
